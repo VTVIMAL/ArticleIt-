11590c7614eb92b9e30eb495a6df87f164e0dd4c
Vimal Prasad V T <vimal.vt.prasad@gmail.com>
2022-12-27 21:07:46 +0530

    Display Post

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+
+
+from .models import Post
 # Create your views here.
+class HomePage(ListView):
+    ''' View for the home page of the website. If a user is authenticated shows article that belongs to the profile he/she follows 
+    else shows all the posts '''
+    model = Post
+    context_object_name = "posts"
+    common_tags = Post.tags.most_common()
+    # catagory = Catagory.objects.all()
+    template_name = "post_list.html"
+    ordering = ["-date_created"]
